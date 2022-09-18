3992813e911442f26ea32a481c826f4ae737630b
Basu1735@ <basu1735@gmail.com>
2022-09-18 13:48:22 +0530

    restriction on notification

--- client/src/pages/Notification.jsx
+++ client/src/pages/Notification.jsx
@@ -1,46 +1,80 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Swal from "sweetalert2";
+import { Link, useNavigate } from "react-router-dom";
 import Header from "../components/header/Header";
 
 const Notification = () => {
+  const navigate = useNavigate();
+  const [isLogin, setIsLogin] = useState(false);
+  // Getting root user data
+
+  const getRootUser = async () => {
+    try {
+      const res = await axios.get("http://localhost:4000/api/user/islogin", {
+        withCredentials: true,
+      });
+      console.log(res);
+      if (res.status === 200) {
+        setIsLogin(true);
+      }
+    } catch (err) {
+      setIsLogin(false);
+      Swal.fire({
+        title: "Please Log in",
+        icon: "warning",
+        confirmButtonText: "OK",
+      })
+        .then((res) => navigate("/login"))
+        .catch((err) => console.log(err));
+    }
+  };
+
+  useEffect(() => {
+    getRootUser();
+  }, []);
   return (
     <>
       <Header />
-      <div class="container d-flex flex-column align-items-center justify-content-center">
-        <Link to="/chat/1234">
-          <div class="card-notification mt-5 p-3 ">
-            <div class="media">
-              <img src="https://imgur.com/3UmlFEf.png" class="mr-3" />
-              <div class="media-body">
-                <h6 class="mt-2 mb-0">You have received 1 message</h6>
-                <small class="text">Mar 20,2020</small>
+      {isLogin ? (
+        <>
+          <div class="container d-flex flex-column align-items-center justify-content-center">
+            <Link to="/chat/1234">
+              <div class="card-notification mt-5 p-3 ">
+                <div class="media">
+                  <img src="https://imgur.com/3UmlFEf.png" class="mr-3" />
+                  <div class="media-body">
+                    <h6 class="mt-2 mb-0">You have received 1 message</h6>
+                    <small class="text">Mar 20,2020</small>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        </Link>
-        <Link to="/chat/1235">
-          <div class="card-notification mt-5 p-3 ">
-            <div class="media">
-              <img src="https://imgur.com/3UmlFEf.png" class="mr-3" />
-              <div class="media-body">
-                <h6 class="mt-2 mb-0">You have received 1 message</h6>
-                <small class="text">Mar 20,2020</small>
+            </Link>
+            <Link to="/chat/1235">
+              <div class="card-notification mt-5 p-3 ">
+                <div class="media">
+                  <img src="https://imgur.com/3UmlFEf.png" class="mr-3" />
+                  <div class="media-body">
+                    <h6 class="mt-2 mb-0">You have received 1 message</h6>
+                    <small class="text">Mar 20,2020</small>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        </Link>
-        <Link to="/chat/1236">
-          <div class="card-notification mt-5 p-3 ">
-            <div class="media">
-              <img src="https://imgur.com/3UmlFEf.png" class="mr-3" />
-              <div class="media-body">
-                <h6 class="mt-2 mb-0">You have received 1 message</h6>
-                <small class="text">Mar 20,2020</small>
+            </Link>
+            <Link to="/chat/1236">
+              <div class="card-notification mt-5 p-3 ">
+                <div class="media">
+                  <img src="https://imgur.com/3UmlFEf.png" class="mr-3" />
+                  <div class="media-body">
+                    <h6 class="mt-2 mb-0">You have received 1 message</h6>
+                    <small class="text">Mar 20,2020</small>
+                  </div>
+                </div>
               </div>
-            </div>
+            </Link>
           </div>
-        </Link>
-      </div>
+        </>
+      ) : null}
     </>
   );
 };
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+import { Link, useNavigate } from "react-router-dom";
 import Header from "../components/header/Header";
 import "react-tabs/style/react-tabs.css";
 import Newsletters from "../components/layouts/Newsletters";
@@ -9,6 +10,8 @@ import img1 from "../assets/image1.png";
 
 const Profile = () => {
   const [userData, setUserData] = useState({});
+  const navigate = useNavigate();
+  const [isLogin, setIsLogin] = useState(false);
 
   // Getting root user data
 
@@ -19,9 +22,19 @@ const Profile = () => {
       });
       console.log(res);
       if (res.status === 200) {
+        setIsLogin(true);
         setUserData(res.data.user);
       }
-    } catch (err) {}
+    } catch (err) {
+      setIsLogin(false);
+      Swal.fire({
+        title: "Please Log in",
+        icon: "warning",
+        confirmButtonText: "OK",
+      })
+        .then((res) => navigate("/login"))
+        .catch((err) => console.log(err));
+    }
   };
 
   useEffect(() => {
@@ -30,107 +43,111 @@ const Profile = () => {
   return (
     <div className="item-details">
       <Header />
-      <section className="fl-page-title">
-        <div className="overlay"></div>
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12">
-              <div className="page-title-inner flex">
-                <div className="page-title-heading">
-                  <h2 className="heading">My Profile</h2>
+      {isLogin ? (
+        <>
+          {" "}
+          <section className="fl-page-title">
+            <div className="overlay"></div>
+            <div className="container">
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="page-title-inner flex">
+                    <div className="page-title-heading">
+                      <h2 className="heading">My Profile</h2>
+                    </div>
+                    <div className="breadcrumbs">
+                      <ul>
+                        <li>
+                          <Link to="/">Home</Link>
+                        </li>
+                        <li>My Profile</li>
+                      </ul>
+                    </div>
+                  </div>
                 </div>
-                <div className="breadcrumbs">
-                  <ul>
-                    <li>
-                      <Link to="/">Home</Link>
-                    </li>
-                    <li>My Profile</li>
-                  </ul>
+              </div>
+            </div>
+          </section>
+          <section className="tf-section item-details-page">
+            <div className="container">
+              <div className="row">
+                <div className="col-xl-4 col-lg-12 col-md-12">
+                  <div className="profile-pic">
+                    <img src={userData.profileimage} alt="profile image" />
+                    <Link to="/editprofile/pic">
+                      <button
+                        name="submit"
+                        type="submit"
+                        style={{ backgroundColor: "#dc39fc00" }}
+                      >
+                        <span>Edit</span>{" "}
+                      </button>
+                    </Link>
+                  </div>
+                </div>
+                <div className="col-xl-8 col-lg-12 col-md-12">
+                  <div style={{ display: "flex", paddingBottom: "10px" }}>
+                    <h2 style={{ marginRight: "20px" }}>
+                      {userData.firstName + " " + userData.lastName}
+                    </h2>
+                    <Link to="/editprofile/info">
+                      <button
+                        name="submit"
+                        type="submit"
+                        style={{ backgroundColor: "#dc39fc00" }}
+                      >
+                        <span>Edit</span>{" "}
+                      </button>
+                    </Link>
+                  </div>
+
+                  <p style={{ fontSize: " 17px" }}>
+                    {" "}
+                    Style is a reflection of your attitude and your personality.
+                  </p>
+                  <hr></hr>
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      flexDirection: "column",
+                    }}
+                  >
+                    <br></br>
+                    <br></br>
+                    <img src={img1} alt="Sell New" />
+                    <p
+                      style={{
+                        fontSize: "20px",
+                        fontweight: "bold",
+                        padding: "10px",
+                      }}
+                    >
+                      {" "}
+                      There are no Ads{" "}
+                    </p>
+                    <p style={{ fontSize: "15px" }}>
+                      When users post ads, will appear here. If you want to post
+                      something you can do it now
+                    </p>
+                    <br></br>
+                    <button
+                      name="submit"
+                      type="submit"
+                      style={{ backgroundColor: "#dc39fc00" }}
+                    >
+                      <span>Start Selling</span>
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-      </section>
-      <section className="tf-section item-details-page">
-        <div className="container">
-          <div className="row">
-            <div className="col-xl-4 col-lg-12 col-md-12">
-              <div className="profile-pic">
-                <img src={userData.profileimage} alt="profile image" />
-                <Link to="/editprofile/pic">
-                  <button
-                    name="submit"
-                    type="submit"
-                    style={{ backgroundColor: "#dc39fc00" }}
-                  >
-                    <span>Edit</span>{" "}
-                  </button>
-                </Link>
-              </div>
-            </div>
-            <div className="col-xl-8 col-lg-12 col-md-12">
-              <div style={{ display: "flex", paddingBottom: "10px" }}>
-                <h2 style={{ marginRight: "20px" }}>
-                  {userData.firstName + " " + userData.lastName}
-                </h2>
-                <Link to="/editprofile/info">
-                  <button
-                    name="submit"
-                    type="submit"
-                    style={{ backgroundColor: "#dc39fc00" }}
-                  >
-                    <span>Edit</span>{" "}
-                  </button>
-                </Link>
-              </div>
-
-              <p style={{ fontSize: " 17px" }}>
-                {" "}
-                Style is a reflection of your attitude and your personality.
-              </p>
-              <hr></hr>
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  flexDirection: "column",
-                }}
-              >
-                <br></br>
-                <br></br>
-                <img src={img1} alt="Sell New" />
-                <p
-                  style={{
-                    fontSize: "20px",
-                    fontweight: "bold",
-                    padding: "10px",
-                  }}
-                >
-                  {" "}
-                  There are no Ads{" "}
-                </p>
-                <p style={{ fontSize: "15px" }}>
-                  When users post ads, will appear here. If you want to post
-                  something you can do it now
-                </p>
-                <br></br>
-                <button
-                  name="submit"
-                  type="submit"
-                  style={{ backgroundColor: "#dc39fc00" }}
-                >
-                  <span>Start Selling</span>
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <Newsletters />
-      <Footer />
+          </section>
+          <Newsletters />
+          <Footer />
+        </>
+      ) : null}
     </div>
   );
 };
